6eca767e58afc79ff8d60f83f1382bc585b8b909
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2025-02-12 08:57:24 +0800

    Update 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py

--- code/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py
+++ code/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.py
@@ -1,29 +1,25 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        # Track current lengths of increasing and decreasing sequences
         incread_max_len = 1
         decread_max_len = 1
+
+        # Track current max lengths
         final_max_len = 1
 
-        pointer = 0
-        tmp_len = 1
-        length = len(nums)
+        for i in range(len(nums) - 1):
+            # Each position is judged to increase, decrease or remain unchanged
+            if nums[i + 1] > nums[i]:
+                decread_max_len += 1
+                incread_max_len = 1
+            elif nums[i + 1] < nums[i]:
+                incread_max_len += 1
+                decread_max_len = 1
+            else:
+                decread_max_len = 1
+                incread_max_len = 1
 
-        if length==1:
-            return 1
-
-        for i in nums:
-            if pointer < length-1:
-                if i > nums[pointer+1]:
-                    decread_max_len += 1
-                    incread_max_len = 1
-                elif i < nums[pointer+1]:
-                    incread_max_len += 1
-                    decread_max_len = 1
-                else:
-                    decread_max_len = 1
-                    incread_max_len = 1
-
+            # Record the best result now
             final_max_len = max(final_max_len, incread_max_len, decread_max_len)
-            pointer += 1
 
         return final_max_len
