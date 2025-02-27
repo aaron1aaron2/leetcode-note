40ac81bcfd2a185971784483af00daa66f74f989
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2025-02-27 10:39:26 +0800

    Create 2375-2. Construct Smallest Number From DI String.py

---
+++ code/2375-2. Construct Smallest Number From DI String.py
@@ -0,0 +1,34 @@
+# Regulated Brute Force via Recursion | solution of editorial
+# print 
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        self.result = []
+
+        # Start building the sequence by calling the helper function
+        self.build_sequence(0, 0, pattern)
+        # Reverse the final result
+        return "".join(self.result[::-1])
+
+    # Recursively build the sequence
+    def build_sequence(
+        self, current_index: int, current_count: int, pattern: str
+    ) -> int:
+        if current_index != len(pattern):
+            if pattern[current_index] == "I":
+                # If 'I', increment the count and move to the next index
+                print('I', current_index, '|', current_count)
+                self.build_sequence(current_index + 1, current_index + 1, pattern)
+            else:
+                # If 'D', keep the count and move to the next index
+                print('D', current_index, '|', current_count)
+                current_count = self.build_sequence(
+                    current_index + 1, current_count, pattern
+                )
+        
+        print(current_index, '|', current_count, 'D')
+        self.result.append(str(current_count + 1))
+        print('append', current_count + 1)
+
+        # Return the next count for the sequence
+        return current_count + 1
+
