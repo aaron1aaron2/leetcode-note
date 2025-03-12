87910594391adb7a8da68b2a571b797409bf7dc1
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2025-03-12 16:27:19 +0800

    Create 2529-1. Maximum Count of Positive Integer and Negative Integer.py

---
+++ code/2529-1. Maximum Count of Positive Integer and Negative Integer.py
@@ -0,0 +1,12 @@
+# basic solution | just count
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        neg = 0
+        pos = 0
+        for i in nums:
+            if i > 0:
+                pos += 1
+            if i < 0:
+                neg += 1
+        
+        return max(pos, neg)
