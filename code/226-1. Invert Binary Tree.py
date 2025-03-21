a22c965617db23a0c6805ffef0b97b473ccb08ff
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2025-03-21 16:20:12 +0800

    Create 226-1. Invert Binary Tree.py

---
+++ code/226-1. Invert Binary Tree.py
@@ -0,0 +1,21 @@
+# solution by https://leetcode.com/problems/invert-binary-tree/solutions/5513149/video-swap-left-child-and-right-child/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    
+        if not root:
+            return
+        
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
