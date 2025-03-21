732ebb680e9c0eb6fe272fe90974599717c71983
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2025-03-21 14:40:31 +0800

    Create 217-2. Contains Duplicate.py

---
+++ code/217-2. Contains Duplicate.py
@@ -0,0 +1,9 @@
+# simple hash set method 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
