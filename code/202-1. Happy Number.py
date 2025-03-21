f17a8992a2d53923cb429bd242abc5895aada52b
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2025-03-21 13:20:34 +0800

    Create 202-1. Happy Number.py

---
+++ code/202-1. Happy Number.py
@@ -0,0 +1,17 @@
+# my solution
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        record = [n] # record repeat num
+        while n != 1:
+            new_n = 0
+            for i in str(n):
+                new_n += int(i)*int(i)
+            n = new_n
+            
+            if new_n in record:
+                # go into circle
+                return False
+            else:
+                record.append(new_n)
+        #  find 1
+        return True
