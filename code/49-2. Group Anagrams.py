d1aa5491579cf65ba8805f315727b8701763aefb
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2025-04-10 16:36:45 +0800

    Create 49-2. Group Anagrams.py

---
+++ code/49-2. Group Anagrams.py
@@ -0,0 +1,10 @@
+# sample method using sort & defaultdict
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
